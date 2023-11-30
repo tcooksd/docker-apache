5f06fca8cbfdd680ef41751c307e7d1610ea2137
Tim Cook <tcooksd@gmail.com>
2023-11-30 14:58:22 -0500

    updating images and adding automation for ease of testing '

---
+++ rebuild.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+check_command_status() {
+    local command_name="$1"
+    if [ $? -ne 0 ]; then
+        echo "Error: $command_name failed"
+        exit 1
+    fi
+}
+
+docker ps | grep my-apache-server > /dev/null
+if [ $? -eq 0 ]; then 
+  running_pid=$(docker ps | awk '{print $1}' | grep -v CON)
+  echo "################################################"
+  echo "Docker is already running my-apache-server, I  #"
+  echo "am stopping docker process id $running_pid now #"
+  echo "################################################"
+  echo ""
+  docker stop $running_pid > /dev/null
+  check_command_status "Stopping docker failed"
+fi 
+
+docker build -t my-apache-server .  > /dev/null 2>&1 &
+check_command_status "Container build"
+
+
+docker run -p 8080:80 my-apache-server > /dev/null 2>&1 &
+check_command_status "docker run" 
+
+open "http://localhost:8080" 
+check_command_status "Chrome open" 
+ 
+sleep 5 
+
+get_dockerid=$(docker ps | grep my-apache-server | awk '{print $1}')
+
+
+echo "################################################"
+echo "If you want to stop the docker process!        #"
+echo "################################################"
+echo ""
+echo "$ docker stop $get_dockerid"  
+echo "or you can just rerun this script and get the same effect"
+echo "################################################"
+
